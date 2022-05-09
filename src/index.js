1f922459b40e52e9c89eba9b20e7789c013a6669
emanoelantonio <dev.emanoel@protonmail.com>
2022-05-09 18:51:42 -0300

    Primeiro post funcionando

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
+app.use(express.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => { 
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,3 +1,4 @@
+const { v4: uuid } = require("uuid");
 const workout = require("../database/workout");
 
 const getAllWorkouts = () => {
@@ -10,8 +11,18 @@ const getOneWorkout = () => {
   return;
 }
 
-const createNewWorkout = () => {
-  return;
+const createNewWorkout = (newWorkout) => {
+
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleDateString("pt-BR", { timeZone: "UTC" }),
+    updatedAt: new Date().toLocaleDateString("pt-BR", { timeZone: "UTC" }),
+  };
+
+  const createdWorkout = workout.createNewWorkout(workoutToInsert);
+  
+  return createdWorkout;
 }
 
 const updateOneWorkout = () => {
