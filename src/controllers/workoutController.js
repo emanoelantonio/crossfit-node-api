5e0ac29629559877b66306c630844a3752211832
emanoelantonio <dev.emanoel@protonmail.com>
2022-05-09 15:46:23 -0300

    Iniciando o controller com os métodos

---
+++ src/controllers/workoutController.js
@@ -0,0 +1,27 @@
+const getAllWorkouts = (req, res) => {
+  res.send("Get all workouts");
+}
+
+const getOneWorkout = (req, res) => {
+  res.send("Get one workout");
+}
+
+const createNewWorkout = (req, res) => {
+  res.send("Create a new workout");
+}
+
+const updateOneOneWorkout = (req, res) => {
+  res.send("Update an existing workout");
+}
+
+const deleteOneWorkout = (req, res) => {
+  res.send("Delete an existing workout");
+}
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneOneWorkout,
+  deleteOneWorkout
+}
