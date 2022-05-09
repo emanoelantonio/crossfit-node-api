d4be15c429dc94f2f79e9be490d58f55bdf77317
emanoelantonio <dev.emanoel@protonmail.com>
2022-05-09 15:56:12 -0300

    v1 pronto

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 const express = require("express"); 
-const v1Router = require("./v1/routes");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
-// Apenas para fins de teste. 
-// app.get("/", (req, res) => { 
-//     res.send("<h2>Tudo ok com o servidor!</h2>"); 
-// }); 
-app.use("/api/v1", v1Router);
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => { 
     console.log(`Servidor executando na porta: ${PORT}`); 
