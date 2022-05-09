b8464f8a2e410539077d95ac333f9388df3acdcc
emanoelantonio <dev.emanoel@protonmail.com>
2022-05-09 15:32:07 -0300

    nova versão da API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express"); 
+const v1Router = require("./v1/routes");
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
 // Apenas para fins de teste. 
-app.get("/", (req, res) => { 
-    res.send("<h2>Tudo ok com o servidor!</h2>"); 
-}); 
+// app.get("/", (req, res) => { 
+//     res.send("<h2>Tudo ok com o servidor!</h2>"); 
+// }); 
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => { 
     console.log(`Servidor executando na porta: ${PORT}`); 
